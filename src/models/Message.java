6284ec5e599b063acaaf711ceebfb392a5a69e6b
ryo.shimoyama <RIYOU.SHIMOYAMA@grp.tmnf.jp>
2023-06-01 14:28:55 +0900

    「ページネーションを追加」

--- src/models/Message.java
+++ src/models/Message.java
@@ -13,11 +13,14 @@ import javax.persistence.Table;
 @Entity
 @NamedQueries({
     @NamedQuery(
-        name = "getAllMessages",
-        query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
-    )
-})
-@Table(name = "messages")
+            name = "getAllMessages",
+            query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+            ),
+    @NamedQuery(
+            name = "getMessagesCount",
+            query = "SELECT COUNT(m) FROM Message AS m"
+            )
+})@Table(name = "messages")
 public class Message {
     @Id
     @Column(name = "id")
